68f8895af7e751d6c86bf215f364235ef102e061
bonnerac <alex.c.bonner@gmail.com>
2020-01-28 20:16:01 -0600

    added code for orm, controllers, and models

---
+++ config/orm.js
@@ -0,0 +1,48 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+// * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+// * `selectAll()`
+// * `insertOne()`
+// * `updateOne()`
+
+var orm = {
+    selectAll: function (tableInput) {
+        var queryString = "SELECT * FROM ?? ";
+
+        connection.query(queryString, [tableInput], function (err, result) {
+            if (err) throw err;
+            return result;
+        });
+    },
+
+
+    // insertOne: function (tableInput) {
+    //     var queryString = "SELECT * FROM ?? ";
+
+    //     connection.query(queryString, [tableInput], function (err, result) {
+    //         if (err) throw err;
+    //         return result;
+    //     });
+    // },
+
+
+
+    // updateOne: function (tableInput) {
+    //     var queryString = "SELECT * FROM ?? ";
+
+    //     connection.query(queryString, [tableInput], function (err, result) {
+    //         if (err) throw err;
+    //         return result;
+    //     });
+    // }
+}
+
+
+
+
+
+
+// Export the orm object.
+module.exports = orm;
